60bb2cc9e0b52d19a7258557cef1c0051b269fd5
Infarh <shmachilin@yandex.ru>
2020-03-01 13:00:57 +0300

    Переработка TCP-сервера

--- MathCore.NET/TCP/Server.cs
+++ MathCore.NET/TCP/Server.cs
@@ -1,14 +1,17 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using MathCore.NET.TCP.Events;
 // ReSharper disable EventNeverSubscribedTo.Global
 // ReSharper disable UnusedType.Global
 // ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedMember.Global
 
 namespace MathCore.NET.TCP
 {
@@ -39,8 +42,8 @@ namespace MathCore.NET.TCP
         protected virtual void OnClientConnected(ClientEventArgs e) =>
             ClientConnected?.Invoke(this, e);
 
-        private void OnClientConnected(object Sender) =>
-            OnClientConnected(new ClientEventArgs((Client)Sender));
+        private void OnClientConnected(Client Client) =>
+            OnClientConnected(new ClientEventArgs(Client));
 
         /// <summary>Событие, возникающие при отключении клиента</summary>
         public event EventHandler<ClientEventArgs> ClientDisconnected;
@@ -50,8 +53,8 @@ namespace MathCore.NET.TCP
         protected virtual void OnClientDisconnected(ClientEventArgs e) =>
             ClientDisconnected?.Invoke(this, e);
 
-        private void OnClientDisconnected(object Sender) =>
-            OnClientDisconnected(new ClientEventArgs((Client)Sender));
+        private void OnClientDisconnected(Client Client) =>
+            OnClientDisconnected(new ClientEventArgs(Client));
 
         /// <summary>Событие, возникающие при получении данных подключённым клиентом</summary>
         public event EventHandler<ClientDataEventArgs> DataReceived;
@@ -59,10 +62,10 @@ namespace MathCore.NET.TCP
         /// <summary>Метод вызова события "при получении данных"</summary>
         protected virtual void OnDataReceived(ClientDataEventArgs e) => DataReceived?.Invoke(this, e);
 
-        /// <param name="Sender">Клиент, получивший данные</param>
+        /// <param name="Client">Клиент, получивший данные</param>
         /// <param name="ClientArgs">Полученные данные (надо переписать, поменяв на полученный от клиента аргумент соответствующего события)</param>
-        private void OnDataReceived(object Sender, DataEventArgs ClientArgs) =>
-            OnDataReceived(new ClientDataEventArgs((Client)Sender, ClientArgs));
+        private void OnDataReceived(Client Client, DataEventArgs ClientArgs) =>
+            OnDataReceived(new ClientDataEventArgs(Client, ClientArgs));
 
         /// <summary>Событие, возникающие при отправке данных подключённым клиентом</summary>
         public event EventHandler<ClientDataEventArgs> DataSent;
@@ -70,10 +73,10 @@ namespace MathCore.NET.TCP
         /// <summary>Метод вызова события "при передаче данных"</summary>
         protected virtual void InvokeDataSendEvent(ClientDataEventArgs e) => DataSent?.Invoke(this, e);
 
-        /// <param name="Sender">Клиент, инициировавший передачу</param>
+        /// <param name="Client">Клиент, инициировавший передачу</param>
         /// <param name="ClientArgs">Параметры передачи</param>
-        private void InvokeDataSendEvent(object Sender, DataEventArgs ClientArgs) =>
-            InvokeDataSendEvent(new ClientDataEventArgs((Client)Sender, ClientArgs));
+        private void InvokeDataSendEvent(Client Client, DataEventArgs ClientArgs) =>
+            InvokeDataSendEvent(new ClientDataEventArgs(Client, ClientArgs));
 
         /// <summary>Событие, возникающие при возникновении ошибки</summary>
         public event EventHandler<ErrorEventArgs> Error;
@@ -96,7 +99,7 @@ namespace MathCore.NET.TCP
         private readonly object _SyncRoot = new object();
 
         /// <summary>Поле, содержащее текущий прослушиваемый порт</summary>
-        protected int _Port;
+        protected readonly int _Port;
 
         protected readonly Encoding _DataEncoding = Encoding.UTF8;
 
@@ -106,17 +109,13 @@ namespace MathCore.NET.TCP
         /// <summary>Основной элемент сервера, производящий прослушивание порта и подключения входящих клиентов</summary>
         protected TcpListener _Listener;
 
-        /// <summary>Главный поток сервера</summary>
-        protected Thread _ServerThread;
+        private CancellationTokenSource _ListenProcessCancellation;
 
         /// <summary>Поле, содержащее информацию о активности сервера</summary>
         protected bool _Enabled;
 
-        /// <summary>Mutex синхронизации процесса асинхронного подключения клиентов</summary>
-        protected ManualResetEvent _ClientConnection;
-
         /// <summary>Список подключённых клиентов</summary>
-        protected List<Client> _ClientList;
+        protected List<Client> _Clients;
 
         #endregion
 
@@ -126,24 +125,12 @@ namespace MathCore.NET.TCP
         /// <value>подключён / отключён</value>
         public bool Enabled { get => _Enabled; set { if (value) Start(); else Stop(); } }
 
-        /// <summary>Свойство, указывающее прослушиваемый порт</summary>
-        /// <value>Должно быть целым в пределах от 1 до 2^16</value>
-        public int Port
-        {
-            get => _Port;
-            set
-            {
-                if (value == _Port) return;
+        /// <summary>Прослушиваемый порт</summary>
+        public int Port => _Port;
 
-                if (value < 1 || value > 65535)
-                    throw new ArgumentOutOfRangeException(nameof(Port), value, $"Порт должен быть в пределах от 1 до 65535, а указан {value}");
-
-                var is_enabled = Enabled;
-                Enabled = false;
-                _Port = value;
-                Enabled = is_enabled;
-            }
-        }
+        /// <summary>
+        /// Система IP адресов</summary>
+        public IPAddress AddressType => _AddressType;
 
         #endregion
 
@@ -151,98 +138,77 @@ namespace MathCore.NET.TCP
 
         /// <summary>Конструктор с указанием прослушиваемого порта</summary>
         /// <param name="Port">Прослушиваемый порт</param>
-        public Server(int Port)
-        {
-            if (Port < 1 || Port > 65535)
-                throw new ArgumentOutOfRangeException(nameof(Port), Port, "Порт должен быть в пределах от 1 до 65535, а указан " + Port.ToString());
-            _Port = Port;
-        }
+        public Server(int Port) => _Port = Port < 1 || Port > 65535
+            ? throw new ArgumentOutOfRangeException(nameof(Port), Port,
+                $"Порт должен быть в пределах от 1 до 65535, а указан {Port}")
+            : Port;
 
         /// <summary>Конструктор с указанием прослушиваемого порта и типа обслуживаемых подсетей</summary>
         /// <param name="Port">Прослушиваемый порт</param>
         /// <param name="AddressType">Система IP адресов</param>
-        public Server(int Port, IPAddress AddressType)
-        {
-            if (Port < 1 || Port > 65535)
-                throw new ArgumentOutOfRangeException(nameof(Port), Port, "Порт должен быть в пределах от 1 до 65535, а указан " + Port.ToString());
-            _Port = Port;
-            _AddressType = AddressType;
-        }
+        public Server(int Port, IPAddress AddressType) : this(Port) => _AddressType = AddressType;
 
         #endregion
 
         #region Запуск / остановка
 
         /// <summary>Метод запуска сервера</summary>
-        protected void Start()
+        public void Start()
         {
             //Если сервер активен, выходим
-            if (Enabled) return;
+            if (_Enabled) return;
             lock (_SyncRoot)
-            {
-                if (Enabled) return;
-                try
-                {
-                    //Устанавливаем признак активности сервера
-                    _Enabled = true;
+                if (!_Enabled)
+                    try
+                    {
+                        //Устанавливаем признак активности сервера
+                        _Enabled = true;
 
-                    //Создаём новый экземпляр "слушателя"
-                    _Listener = new TcpListener(_AddressType, _Port);
-                    _Listener.Start();
-                    //Создаём блокирующий объект для синхронизации потоков
-                    _ClientConnection = new ManualResetEvent(false);
-                    //Создаём список обслуживаемых клиентов
-                    _ClientList = new List<Client>(10);
+                        //Создаём новый экземпляр "слушателя"
+                        _Listener = new TcpListener(_AddressType, _Port);
+                        _Listener.Start();
+                        //Создаём список обслуживаемых клиентов
+                        _Clients = new List<Client>();
 
-                    //Создаём основной поток обработки подключений
-                    _ServerThread = new Thread(Listen);
-                    //Запускаем сервер
-                    _ServerThread.Start();
+                        _ListenProcessCancellation = new CancellationTokenSource();
+                        ListenAsync(_Listener, _ListenProcessCancellation.Token);
 
-                    OnStarted();
-                }
-                catch (SocketException error)
-                {
-                    Stop();
-                    InvokeErrorEvent(error);
-                }
-            }
+                    }
+                    catch (SocketException error)
+                    {
+                        Stop();
+                        InvokeErrorEvent(error);
+                    }
+                else return;
+            OnStarted();
         }
 
         /// <summary>Метод остановки сервера</summary>
-        protected void Stop()
+        public void Stop()
         {
             //Если сервер неактивен, то выходим
-            if (!Enabled) return;
+            if (!_Enabled) return;
             lock (_SyncRoot)
-            {
-                if (!Enabled) return;
-                _Enabled = false;
-
-                if (_ClientList != null)
-                    for (var i = 0; i < _ClientList.Count; i++)
-                        _ClientList[i].Enabled = false;
-
-                //Останавливаем слушателя
-                _Listener.Stop();
-                _ClientConnection?.Set();
-
-                //Если поток всё ещё активен, то...
-                if (_ServerThread?.IsAlive == true)
+                if (_Enabled)
                 {
-                    //Вызываем остановку потока вручную.
-                    _ServerThread.Abort();
-                    //Ожидаем завершения потока (синхронизация)
-                    _ServerThread.Join();
+                    //Устанавливаем признак активности сервера в состояние "отключён"
+                    _Enabled = false;
+                    _ListenProcessCancellation.Cancel();
+                    _ListenProcessCancellation.Dispose();
+
+                    if (_Clients != null)
+                        foreach (var client in _Clients)
+                            client.Enabled = false;
+
+                    //Останавливаем слушателя
+                    _Listener.Stop();
+
+                    //Обнуляем ссылки
+                    _Clients = null;
+                    _Listener = null;
+                    _ListenProcessCancellation = null;
                 }
-
-                //Обнуляем ссылки
-                _ServerThread = null;
-                _Listener = null;
-                _ClientConnection = null;
-            }
-
-            //Устанавливаем признак активности сервера в состояние "отключён"
+                else return;
 
             OnStopped();
         }
@@ -251,58 +217,37 @@ namespace MathCore.NET.TCP
 
         #region Обработка подключений
 
-        /// <summary>Главный метод сервера.</summary>
-        /// <remarks>
-        /// В нём производиться асинхронная обработка подключений клиентов
-        /// Стартует в отдельном потоке
-        /// </remarks>
-        protected void Listen()
+        protected virtual async void ListenAsync(TcpListener Listener, CancellationToken Cancel)
         {
-            //Основной цикл сервера. Предназначен для определения факта подключения
-            while (_Enabled) //Признак продолжения работы - активность сервера
-            {
-                try //Блок обработки ошибок
-                {
-                    _ClientConnection.Reset();
-                    //Асинхронный захват подключения с передачей дальнейшей обработки в метод void ClientAccepted(IAsyncResult AsyncResult)
-                    _Listener.BeginAcceptTcpClient(ClientAccepted, _Listener);
-                    //Ожидаем флага разрешения дальнейшей работы
-                    _ClientConnection.WaitOne();
-                }
-                catch (Exception error) //Перехват остальных ошибок
-                {
-                    InvokeErrorEvent(error);
-                }
-            }
-        }
-
-        /// <summary>Метод асинхронного завершения подключения клиента</summary>
-        protected void ClientAccepted(IAsyncResult AsyncResult)
-        {
-            var listener = (TcpListener)AsyncResult.AsyncState;
-
-            if (!Enabled) return;
-
-            //Обработка исключительных ситуаций (какая умная фраза!)
             try
             {
-                //Получаем подключившегося клиента
-                InitializeClient(listener.EndAcceptTcpClient(AsyncResult));
+                TcpClient client = null;
+                while (true)
+                {
+                    Cancel.ThrowIfCancellationRequested();
+                    var waiting_client_task = Listener
+                       .AcceptTcpClientAsync()
+                       .WithCancellation(Cancel)
+                       .ConfigureAwait(false);
+
+                    if (client != null) AcceptClient(client);
+
+                    client = await waiting_client_task;
+                }
             }
+            catch (OperationCanceledException) { }
             catch (Exception error)
             {
                 InvokeErrorEvent(error);
             }
-            //Разрешение на дальнейшую обработку подключений клиента
-            _ClientConnection.Set();
         }
 
-        protected virtual void InitializeClient(TcpClient Client)
+        protected virtual void AcceptClient(TcpClient Client)
         {
             //Создаём новый экземпляр класса "Client", в котором будет происходить дальнейшая работа с клиентом
             var client = new Client(Client);
 
-            //подписываемся на обработчики событий клиента
+            //подписываемся на события клиента
             client.Connected += OnClientConnected;
             client.Disconnected += OnClientDisconnected;
             client.DataReceived += OnClientDataReceived;
@@ -312,8 +257,22 @@ namespace MathCore.NET.TCP
             client.DataEncoding = _DataEncoding;
 
             //Добавляем клиента в список
-            _ClientList.Add(client);
+            lock (_Clients) _Clients.Add(client);
             OnClientConnected(client);
+        }
+
+        protected virtual void DisconnectClient(Client Client)
+        {
+            //отписываемся от событий клиента
+            Client.Connected -= OnClientConnected;
+            Client.Disconnected -= OnClientDisconnected;
+            Client.DataReceived -= OnClientDataReceived;
+            Client.Error -= OnClientError;
+            Client.DataSent -= OnClientDataSent;
+
+            //Удаляем клиента из списка
+            lock (_Clients) _Clients.Remove(Client);
+            OnClientDisconnected(Client);
         }
 
         #endregion
@@ -324,7 +283,7 @@ namespace MathCore.NET.TCP
         /// <param name="Sender">Клиент, отправивший данные</param>
         /// <param name="Args">Параметры</param>
         private void OnClientDataSent(object Sender, DataEventArgs Args) =>
-            InvokeDataSendEvent(Sender, Args);
+            InvokeDataSendEvent((Client) Sender, Args);
 
         /// <summary>Метод обработки событий подключённых клиентов "при ошибке"</summary>
         /// <param name="Sender">Клиент, совершивший ошибку</param>
@@ -336,27 +295,22 @@ namespace MathCore.NET.TCP
         /// <summary>Метод обработки событий подключённых клиентов "при получении данных"</summary>
         /// <param name="Sender">Клиент, получивший данные</param>
         /// <param name="Args">Параметры</param>
-        private void OnClientDataReceived(object Sender, DataEventArgs Args) => 
-            OnDataReceived(Sender, Args);
+        private void OnClientDataReceived(object Sender, DataEventArgs Args) =>
+            OnDataReceived((Client) Sender, Args);
 
         /// <summary>Метод обработки событий подключённых клиентов "при отключении"</summary>
         /// <param name="Sender">Отключившийся клиент</param>
         /// <param name="Args">Параметры</param>
-        private void OnClientDisconnected(object Sender, EventArgs Args)
-        {
-            var client = (Client)Sender;
-            _ClientList.Remove(client);
-            OnClientDisconnected(client);
-        }
+        private void OnClientDisconnected(object Sender, EventArgs Args) => DisconnectClient((Client)Sender);
 
         /// <summary>Метод обработки событий подключённых клиентов "при подключении"</summary>
         /// <param name="Sender">Подключившийся клиент</param>
         /// <param name="Args">Параметры</param>
-        private void OnClientConnected(object Sender, EventArgs Args) => OnClientConnected(Sender);
+        private void OnClientConnected(object Sender, EventArgs Args) => OnClientConnected((Client)Sender);
 
         #endregion
 
-        public override string ToString() => $"TCP:{Port}";
+        public override string ToString() => $"tcp://{_AddressType}:{Port}";
 
         #region IDispose implementation
 
@@ -377,10 +331,10 @@ namespace MathCore.NET.TCP
             if (_Disposed || !disposing) return;
             _Disposed = true;
             Stop();
-            _ClientConnection?.Dispose();
+            _ListenProcessCancellation?.Dispose();
+            lock (_Clients) _Clients?.Clear();
         }
 
         #endregion
-
     }
 }
