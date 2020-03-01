4f3432cc7085b479d20a2beee2b44d460bc55a34
Infarh <shmachilin@yandex.ru>
2020-03-01 11:34:28 +0300

    Тестовые проекты

---
+++ Tests/MathCore.NET.TestConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathCore.NET.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
